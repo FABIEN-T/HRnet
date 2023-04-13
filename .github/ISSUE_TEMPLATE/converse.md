dfe4687b5ef4661ac7ffd48400cc6dc9129e713a
fabT <fabien.varlet@live.fr>
2023-04-13 16:00:55 +0200

    initialisation et import essai package

---
+++ .github/ISSUE_TEMPLATE/converse.md
@@ -0,0 +1,17 @@
+---
+name: '#1 Landing Page'
+about: Issue for the landing page requirement
+title: User can see the home page
+labels: ''
+assignees: ''
+---
+
+| name            |  about   |           title | labels | assignees |
+| :-------------- | :------: | --------------: | :----: | --------: |
+| # Conversion of the entire HRNet project to React | issue  | Aligné à droite |
+
+A user should be able to:
+
+- [ ] Navigate to the home page (`/`)
+- [ ] See all placeholder data from mockup
+
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from 'fv_vite-react-ts-prop-button'
+
+function App() {
+  const [count, setCount] = useState(0)
+  {
+    function addition(x:number, y:number) {
+      return x + y;
+  }
+  const result = addition(12, 14);
+  console.log(result);  
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        {/* <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+      </div>
+      <h1>Vite + React + TS + Styled Components</h1>     
+      <Button text={'Essai props'} btnColor={'yellow'} btnBgColor={'midnightblue'} fontSize={'1.2rem'} />  
+    </div>
+  )
+}
+
+export default App
