356e14bd1ffb19c3ab32642660594ae5363acfc5
fabT <fabien.varlet@live.fr>
2023-04-21 14:33:38 +0200

    réimport package fv-modal-react

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -28,7 +28,7 @@ export default function Form() {
 
   const save = (data) => {
     setIsOpen(true)
-    localStorage.setItem('employee', JSON.stringify(data))
+    localStorage.setItem('employee', data)
     console.log(
       data
       // data.dateBirth ? data.dateBirth.toLocaleDateString() : null,
@@ -36,242 +36,244 @@ export default function Form() {
   }
 
   return (
-    <div className="createEmployee">
-      {/* <div> */}
-      <h1>HRnet</h1>
-      <h2>Create Employee</h2>
-      <form id="external-form">
-        <div className="inputName-wrapper">
-          <div className="inputContainer">
-            <label htmlFor="firstName">First Name</label>
-            <input
-              name="firstName"
-              {...register('firstName', {
-                required: true,
-                maxLength: 20,
-                pattern: /^[a-zA-Z\s\-À-ÖØ-öø-ÿ']+$/g,
-              })}
-            />
-            <div className="inputNameError">
-              {errors?.firstName?.type === 'required' && (
-                <p className="pErrorName">This field is required</p>
-              )}
-              {errors?.firstName?.type === 'maxLength' && (
-                <p className="pErrorName">Cannot exceed 20 characters</p>
-              )}
-              {errors?.firstName?.type === 'pattern' && (
-                <p className="pErrorName">Alphabetical characters only</p>
-              )}
+    <>
+      <div className="createEmployee">
+        {/* <div> */}
+        <h1>HRnet</h1>
+        <h2>Create Employee</h2>
+        <form id="external-form">
+          <div className="inputName-wrapper">
+            <div className="inputContainer">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                name="firstName"
+                {...register('firstName', {
+                  required: true,
+                  maxLength: 20,
+                  pattern: /^[a-zA-Z\s\-À-ÖØ-öø-ÿ']+$/g,
+                })}
+              />
+              <div className="inputNameError">
+                {errors?.firstName?.type === 'required' && (
+                  <p className="pErrorName">This field is required</p>
+                )}
+                {errors?.firstName?.type === 'maxLength' && (
+                  <p className="pErrorName">Cannot exceed 20 characters</p>
+                )}
+                {errors?.firstName?.type === 'pattern' && (
+                  <p className="pErrorName">Alphabetical characters only</p>
+                )}
+              </div>
             </div>
-          </div>
-          <div className="inputContainer">
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              name="lastName"
-              {...register('lastName', {
-                required: true,
-                maxLength: 20,
-                pattern: /^[a-zA-Z\s\-À-ÖØ-öø-ÿ']+$/g,
-              })}
-            />
-            <div className="inputNameError">
-              {errors?.lastName?.type === 'required' && (
-                <p className="pErrorName pLastName">This field is required</p>
-              )}
-              {errors?.lastName?.type === 'maxLength' && (
-                <p className="pErrorName pLastName">
-                  Cannot exceed 20 characters
-                </p>
-              )}
-              {errors?.lastName?.type === 'pattern' && (
-                <p className="pErrorName pLastName">
-                  Alphabetical characters only
-                </p>
-              )}
+            <div className="inputContainer">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                name="lastName"
+                {...register('lastName', {
+                  required: true,
+                  maxLength: 20,
+                  pattern: /^[a-zA-Z\s\-À-ÖØ-öø-ÿ']+$/g,
+                })}
+              />
+              <div className="inputNameError">
+                {errors?.lastName?.type === 'required' && (
+                  <p className="pErrorName pLastName">This field is required</p>
+                )}
+                {errors?.lastName?.type === 'maxLength' && (
+                  <p className="pErrorName pLastName">
+                    Cannot exceed 20 characters
+                  </p>
+                )}
+                {errors?.lastName?.type === 'pattern' && (
+                  <p className="pErrorName pLastName">
+                    Alphabetical characters only
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
 
-          <section>
-            <label htmlFor="dateBirth">Date of Birth</label>
-            <Controller
-              control={control}
-              name="dateBirth"
-              rules={{ required: true }}
-              render={({ field }) => (
-                <DatePicker
-                  className="input"
-                  name="dateBirth"
-                  selected={field.value}
-                  dateFormat="MM/dd/yyyy"
-                  onChange={field.onChange}
-                  peekNextMonth
-                  showMonthDropdown
-                  showYearDropdown
-                  dropdownMode="select"
-                  closeOnScroll={true}
-                  disabledKeyboardNavigation
-                />
-              )}
-            />
-            <div className="inputNameError">
-              {errors?.dateBirth?.type === 'required' && (
-                <p className="pErrorName">This field is required</p>
-              )}
-            </div>
-          </section>
-          <section>
-            <label htmlFor="dateStart">Start Date</label>
-            <Controller
-              control={control}
-              name="dateStart"
-              rules={{ required: true }}
-              render={({ field }) => (
-                <DatePicker
-                  className="input"
-                  name="dateStart"
-                  selected={field.value}
-                  dateFormat="MM/dd/yyyy"
-                  onChange={field.onChange}
-                  peekNextMonth
-                  showMonthDropdown
-                  showYearDropdown
-                  dropdownMode="select"
-                  closeOnScroll={true}
-                  disabledKeyboardNavigation
-                />
-              )}
-            />
-            <div className="inputNameError">
-              {errors?.dateStart?.type === 'required' && (
-                <p className="pErrorName">This field is required</p>
-              )}
-            </div>
-          </section>
-          <div className="frame">
-            <p className="pAddress">Address</p>
-            <div className="address">
-              <div className="inputContainer">
-                <label htmlFor="street">Street</label>
-                <input
-                  name="street"
-                  {...register('street', {
-                    required: true,
-                    maxLength: 60,
-                    pattern: /^[a-zA-Z\s\-À-ÖØ-öø-ÿ'0-9,]+$/g,
-                  })}
-                />
-                <div className="inputNameError">
-                  {errors?.street?.type === 'required' && (
-                    <p className="pErrorName">This field is required</p>
-                  )}
-                  {/* {errors?.street?.type === 'maxLength' && (
+            <section>
+              <label htmlFor="dateBirth">Date of Birth</label>
+              <Controller
+                control={control}
+                name="dateBirth"
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <DatePicker
+                    className="input"
+                    name="dateBirth"
+                    selected={field.value}
+                    dateFormat="MM/dd/yyyy"
+                    onChange={field.onChange}
+                    peekNextMonth
+                    showMonthDropdown
+                    showYearDropdown
+                    dropdownMode="select"
+                    closeOnScroll={true}
+                    disabledKeyboardNavigation
+                  />
+                )}
+              />
+              <div className="inputNameError">
+                {errors?.dateBirth?.type === 'required' && (
+                  <p className="pErrorName">This field is required</p>
+                )}
+              </div>
+            </section>
+            <section>
+              <label htmlFor="dateStart">Start Date</label>
+              <Controller
+                control={control}
+                name="dateStart"
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <DatePicker
+                    className="input"
+                    name="dateStart"
+                    selected={field.value}
+                    dateFormat="MM/dd/yyyy"
+                    onChange={field.onChange}
+                    peekNextMonth
+                    showMonthDropdown
+                    showYearDropdown
+                    dropdownMode="select"
+                    closeOnScroll={true}
+                    disabledKeyboardNavigation
+                  />
+                )}
+              />
+              <div className="inputNameError">
+                {errors?.dateStart?.type === 'required' && (
+                  <p className="pErrorName">This field is required</p>
+                )}
+              </div>
+            </section>
+            <div className="frame">
+              <p className="pAddress">Address</p>
+              <div className="address">
+                <div className="inputContainer">
+                  <label htmlFor="street">Street</label>
+                  <input
+                    name="street"
+                    {...register('street', {
+                      required: true,
+                      maxLength: 60,
+                      pattern: /^[a-zA-Z\s\-À-ÖØ-öø-ÿ'0-9,]+$/g,
+                    })}
+                  />
+                  <div className="inputNameError">
+                    {errors?.street?.type === 'required' && (
+                      <p className="pErrorName">This field is required</p>
+                    )}
+                    {/* {errors?.street?.type === 'maxLength' && (
                   <p className="pErrorName">Cannot exceed 60 characters</p>
                 )} */}
-                  {errors?.street?.type === 'pattern' && (
-                    <p className="pErrorName">Alphabetical characters only</p>
-                  )}
+                    {errors?.street?.type === 'pattern' && (
+                      <p className="pErrorName">Alphabetical characters only</p>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <div className="inputContainer">
-                <label htmlFor="city">City</label>
-                <input
-                  name="city"
-                  {...register('city', {
-                    required: true,
-                    maxLength: 30,
-                    pattern: /^[a-zA-Z\s\-À-ÖØ-öø-ÿ']+$/g,
-                  })}
-                />
-                <div className="inputNameError">
-                  {errors?.city?.type === 'required' && (
-                    <p className="pErrorName">This field is required</p>
-                  )}
-                  {errors?.city?.type === 'maxLength' && (
-                    <p className="pErrorName">Cannot exceed 60 characters</p>
-                  )}
-                  {errors?.city?.type === 'pattern' && (
-                    <p className="pErrorName">Alphabetical characters only</p>
-                  )}
+                <div className="inputContainer">
+                  <label htmlFor="city">City</label>
+                  <input
+                    name="city"
+                    {...register('city', {
+                      required: true,
+                      maxLength: 30,
+                      pattern: /^[a-zA-Z\s\-À-ÖØ-öø-ÿ']+$/g,
+                    })}
+                  />
+                  <div className="inputNameError">
+                    {errors?.city?.type === 'required' && (
+                      <p className="pErrorName">This field is required</p>
+                    )}
+                    {errors?.city?.type === 'maxLength' && (
+                      <p className="pErrorName">Cannot exceed 60 characters</p>
+                    )}
+                    {errors?.city?.type === 'pattern' && (
+                      <p className="pErrorName">Alphabetical characters only</p>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <div className="inputContainer">
-                <label htmlFor="selectState">State</label>
-                <Controller
-                  control={control}
-                  name="selectState"
-                  rules={{ required: true }}
-                  render={({ field }) => (
-                    <Select
-                      className="input"
-                      name="selectState"
-                      defaultValue={null}
-                      selected={field.value}
-                      onChange={field.onChange}
-                      options={labelStates}
-                    />
-                  )}
-                />
-                <div className="inputNameError">
-                  {errors?.selectState?.type === 'required' && (
-                    <p className="pErrorName">This field is required</p>
-                  )}
+                <div className="inputContainer">
+                  <label htmlFor="selectState">State</label>
+                  <Controller
+                    control={control}
+                    name="selectState"
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <Select
+                        className="input"
+                        name="selectState"
+                        defaultValue={null}
+                        selected={field.value}
+                        onChange={field.onChange}
+                        options={labelStates}
+                      />
+                    )}
+                  />
+                  <div className="inputNameError">
+                    {errors?.selectState?.type === 'required' && (
+                      <p className="pErrorName">This field is required</p>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <div className="inputContainer">
-                <label htmlFor="zipcode">Zip Code</label>
-                <input
-                  name="zipcode"
-                  type="number"
-                  {...register('zipcode', {
-                    required: true,
-                    pattern:
-                      /^((\d{5}-\d{4})|(\d{5})|([A-Z]\d[A-Z]\s\d[A-Z]\d))$/g,
-                  })}
-                />
-                <div className="inputNameError">
-                  {errors?.zipcode?.type === 'required' && (
-                    <p className="pErrorName">This field is required</p>
-                  )}
-                  {errors?.zipcode?.type === 'pattern' && (
-                    <p className="pErrorName">5 digits are required</p>
-                  )}
+                <div className="inputContainer">
+                  <label htmlFor="zipcode">Zip Code</label>
+                  <input
+                    name="zipcode"
+                    type="number"
+                    {...register('zipcode', {
+                      required: true,
+                      pattern:
+                        /^((\d{5}-\d{4})|(\d{5})|([A-Z]\d[A-Z]\s\d[A-Z]\d))$/g,
+                    })}
+                  />
+                  <div className="inputNameError">
+                    {errors?.zipcode?.type === 'required' && (
+                      <p className="pErrorName">This field is required</p>
+                    )}
+                    {errors?.zipcode?.type === 'pattern' && (
+                      <p className="pErrorName">5 digits are required</p>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="inputContainer">
-            <label htmlFor="selectDepartement">Departement</label>
-            <Controller
-              control={control}
-              name="selectDepartement"
-              rules={{ required: true }}
-              render={({ field }) => (
-                <Select
-                  className="input"
-                  name="selectDepartement"
-                  defaultValue={null}
-                  selected={field.value}
-                  onChange={field.onChange}
-                  options={labelDepartement}
-                />
-              )}
-            />
-            <div className="inputNameError">
-              {errors?.selectDepartement?.type === 'required' && (
-                <p className="pErrorName">This field is required</p>
-              )}
+            <div className="inputContainer">
+              <label htmlFor="selectDepartement">Departement</label>
+              <Controller
+                control={control}
+                name="selectDepartement"
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <Select
+                    className="input"
+                    name="selectDepartement"
+                    defaultValue={null}
+                    selected={field.value}
+                    onChange={field.onChange}
+                    options={labelDepartement}
+                  />
+                )}
+              />
+              <div className="inputNameError">
+                {errors?.selectDepartement?.type === 'required' && (
+                  <p className="pErrorName">This field is required</p>
+                )}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="buttonDiv">
-          <button className="edit-button" onClick={handleSubmit(save)}>
-            Save
-          </button>
-        </div>
-      </form>
-      {/* </div> */}
+          <div className="buttonDiv">
+            <button className="edit-button" onClick={handleSubmit(save)}>
+              Save
+            </button>
+          </div>
+        </form>
+        {/* </div> */}
+      </div>
       {isOpen && <Modal setIsOpen={setIsOpen} />}
-    </div>
+    </>
   )
 }
